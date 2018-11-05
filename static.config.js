c4e569275399ab88829c4c2dd1b526bb5610c34a
Jon Linnell <linnell.jon@gmail.com>
2018-11-05 14:37:23 +0000

    Remove placeholder blog posts

--- static.config.js
+++ static.config.js
@@ -6,37 +6,20 @@ export default {
   getSiteData: () => ({
     title: 'Jon Linnell',
   }),
-  getRoutes: async () => {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    return [
-      {
-        path: '/',
-        component: 'src/containers/Home',
-      },
-      {
-        path: '/about',
-        component: 'src/containers/About',
-      },
-      {
-        path: '/blog',
-        component: 'src/containers/Blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
-      {
-        is404: true,
-        component: 'src/containers/404',
-      },
-    ]
-  },
+  getRoutes: () => [
+    {
+      path: '/',
+      component: 'src/containers/Home',
+    },
+    {
+      path: '/about',
+      component: 'src/containers/About',
+    },
+    {
+      is404: true,
+      component: 'src/containers/404',
+    },
+  ],
   renderToHtml: (render, Comp, meta) => {
     const sheet = new ServerStyleSheet()
     const html = render(sheet.collectStyles(<Comp />))
