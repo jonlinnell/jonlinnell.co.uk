96754b6d83f9193d2125bdf2cd4d8d47e8e96af8
Jon Linnell <linnell.jon@gmail.com>
2019-03-26 19:04:17 +0000

    Conflicts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -26,33 +25,5 @@ if (typeof document !== 'undefined') {
       // eslint-disable-next-line
       render(require('./App').default);
     });
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Your top level component
-import App from './App';
-
-// Export your top level component as JSX (for static rendering)
-export default App;
-
-// Render your app
-if (typeof document !== 'undefined') {
-  const renderMethod = module.hot
-    ? ReactDOM.render
-    : ReactDOM.hydrate || ReactDOM.render;
-
-  const render = (Comp) => {
-    renderMethod(<Comp />, document.getElementById('root'));
-  };
-
-  // Render!
-  render(App);
-
-  // Hot Module Replacement
-  if (module.hot) {
-    // eslint-disable-next-line
-    module.hot.accept('./App', () => render(require('./App').default));
->>>>>>> deb514e9d209d21ae35d9e641312b1dd0d641459
   }
 }
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { withSiteData } from 'react-static';
-
-export default withSiteData(({ title }) => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Hi, I&apos;m {title}.</h1>
-  </div>
-));
