901fc4b67cde7c6203d5d0fd86a5dacb77a5997f
Jon Linnell <linnell.jon@gmail.com>
2019-03-29 20:51:37 +0000

    Spaces!

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export default styled.a`
-  color: ${ ({ theme: { textPrimary } }) => textPrimary };
+  color: ${({ theme: { textPrimary } }) => textPrimary};
 `;
