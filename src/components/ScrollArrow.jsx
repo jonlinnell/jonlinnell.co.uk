7d7187127801f751d77eff657065aa787b206c3c
Jon Linnell <linnell.jon@gmail.com>
2019-03-31 11:14:36 +0100

    ScrollArrow off-center on mobile devices

--- src/components/ScrollArrow.jsx
+++ src/components/ScrollArrow.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import media from '../style/mediaQueries';
+
 import downArrow from '../images/down.png';
 
 const AnchoredArrow = styled.img`
@@ -12,6 +14,11 @@ const AnchoredArrow = styled.img`
   transition: all ease-in-out 0.3s;
   bottom: ${({ minimised }) => (minimised ? '110vh' : '2vh')};
   opacity: ${({ minimised }) => (minimised ? 0 : 1)};
+
+  ${media.phone`
+    bottom: ${({ minimised }) => (minimised ? '110vh' : '16px')};
+    left: calc(99vw - 32px);
+  `}
 `;
 
 export default ({ minimised }) => (
