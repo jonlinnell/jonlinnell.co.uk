d111f60aae89d65d0fa54efb1dbee753a25ec23a
Jon Linnell <linnell.jon@gmail.com>
2019-04-10 20:44:07 +0100

    Change theme colour

--- static.config.js
+++ static.config.js
@@ -39,7 +39,7 @@ export default {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#ed544a" />
       </Head>
       <Body>{children}</Body>
     </Html>
