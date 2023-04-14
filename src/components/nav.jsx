ca0d7244558470781462a9a2584460d3fb0b74e5
Jon Linnell <jonlinnell@users.noreply.github.com>
2023-04-14 09:53:33 +0100

    Add photo, writing to nav bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 import Link from "next/link";
 import ToggleDarkMode from "./toggle-dark-mode";
 
-export default function Nav({}) {
+export default function Nav({ }) {
   return (
     <header className="p-2 md:p-4 min-w-full flex gap-2 justify-start">
       <Link href="/">
@@ -11,6 +11,8 @@ export default function Nav({}) {
         </span>
       </Link>
       <nav className="ml-auto flex gap-3">
+        <Link href="/articles">writing</Link>
+        <Link href="/photography">photo</Link>
         <Link href="/about">about me</Link>
 
         <ToggleDarkMode />
